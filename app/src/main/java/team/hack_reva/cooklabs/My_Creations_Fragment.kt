5b27762b05680c80ed7e12f1cc39f1bc54f906ab
asv0018 <shreevallabhas@gmail.com>
2020-11-06 14:48:26 +0530

    google-service.json updated

--- app/src/main/java/team/hack_reva/cooklabs/My_Creations_Fragment.kt
+++ app/src/main/java/team/hack_reva/cooklabs/My_Creations_Fragment.kt
@@ -78,50 +78,7 @@ class My_Creations_Fragment : Fragment() {
                         Toast.makeText(context,"You will have to create a lab inorder to see them in my creations section", Toast.LENGTH_SHORT).show()
                     }
                 }
-        /*
-        val firestore = FirebaseFirestore.getInstance().collection("user-cook-labs")
-            .document(authuid)
-            .collection("posts")
-            .get().addOnCompleteListener {
-                if(!it.result?.isEmpty!!){
-                    for (i in it.result!!.documents){
-                        // First is to get the data
-                        Log.d("DATA", "Data is coming boyui")
-                        val storage_path = i.getString("audio-storage-name").toString()
-                        val author = i.getString("author").toString()
-                        val name_of_dish = i.getString("name-of-post").toString()
-                        val no_of_steps = i.getDouble("no-of-steps")?.toDouble()
-                        var cover_pic_path = i.getString("cover-picture-path").toString()
-                        cover_pic_path = cover_pic_path.replace(".jpg", "")
-                        val uid_of_author = i.getString("uid-of-author").toString()
-                        Log.d("TEST", cover_pic_path)
-                        // Then download the necessary items to show it up there in the recyclerview
-                        var url_of_cover_pic = ""
-                        photo_storage.child(cover_pic_path).downloadUrl.addOnFailureListener {
-                            Log.d("DOWNLOAD", "Unable to download as it failed in downloading images")
-                        }.addOnSuccessListener {
-                            url_of_cover_pic = it.toString()
-                        }
-                        Log.d("CHECK", "Url of cover pic $url_of_cover_pic")
-                        var url_of_profile_pic = ""
-                        val post_pic = FirebaseStorage.getInstance().reference
-                        post_pic.child("user-accounts/profile-pictures/$uid_of_author").downloadUrl.addOnSuccessListener {
-                            url_of_profile_pic = it.toString()
-                            posts_list.add(Posts(url_of_profile_pic,url_of_cover_pic,name_of_dish,"user-cook-labs/$authuid/posts/$name_of_dish"))
-                            postsAdapter.notifyDataSetChanged()
-                        }
-
-
-                    }
-                }else{
-                    Toast.makeText(context,"You will have to create a lab inorder to see them in my creations section", Toast.LENGTH_SHORT).show()
-                }
-            }.addOnFailureListener{
-                Log.d("TAG", "There is an exception in the fire store reading the data")
-            }*/
-
-
-
+        
         postsAdapter.notifyDataSetChanged()
         return view
     }
---
+++ app/src/main/java/team/hack_reva/cooklabs/PlayCookLabs.kt
@@ -0,0 +1,11 @@
+package team.hack_reva.cooklabs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayCookLabs : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_cook_labs)
+    }
+}
